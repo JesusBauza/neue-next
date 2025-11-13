1e22edba8db69469bf2152581dbab9b59cbaf44a
Jesus Bauza <jesusbauza81@gmail.com>
2025-11-13 13:21:10 -0400

    cambio about

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -12,7 +12,7 @@ export default function About() {
         Creative studio based on Margarita Island, shaking up the established
         and designing with differentiation as our true north. We believe in
         challenging conventions and crafting bold identities that make brands
-        impossible to ignore. With over 16 trusted brand partnerships under our
+        impossible to ignore. With over 20 trusted brand partnerships under our
         belt, we&apos;ve proven that great design isn&apos;t just about looking
         good—it&apos;s about standing out in a crowded world. Available
         Worldwide.
